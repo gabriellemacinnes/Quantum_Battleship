2a9fc84ad8d2db3555c76d6b228eb0a83121c73c
rianadutta <riana.dutta@mail.mcgill.ca>
2023-11-04 21:05:58 -0400

    l

---
+++ board.py.py
@@ -0,0 +1,33 @@
+import random
+import pandas
+import numpy as np
+import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, transpile, execute, Aer
+
+from qiskit.visualization import plot_bloch_multivector
+from qiskit.tools.visualization import plot_histogram
+
+#returns a generated board of qubits
+def generate_board(dim, ships, boards):
+    b = [[0 for _ in range(dim)] for _ in range(dim)]
+    target = ships*boards
+    current = 0
+    m = []
+    for i in range(dim*dim):
+        random_number = random.uniform(0, min(boards - boards/3 , target - current ))
+        m.append( random_number / boards)
+        current += random_number
+    random.shuffle(m)
+    for i in range(dim):
+        for j in range(dim):
+            prob_0 = m[i*dim + j]
+            amplitude_0 = np.sqrt(prob_0)
+            amplitude_1 = np.sqrt(1 - prob_0)
+            qr = QuantumRegister(1, 'q')
+            circuit = QuantumCircuit(qr)
+            circuit.initialize([amplitude_0, amplitude_1], qr[0])
+            b[i][j] = circuit
+    return b
+
+
+ 
