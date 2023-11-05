56d7a44d281ad8fdaacd23c9cce24c451d5aa04b
rianadutta <riana.dutta@mail.mcgill.ca>
2023-11-04 21:45:32 -0400

    Commit 1.0.0.0: changed line 32 board.py to ensure probability distribution:

--- board.py
+++ board.py
@@ -2,7 +2,7 @@ import random
 import pandas
 import numpy as np
 import qiskit
-from qiskit import QuantumCircuit, QuantumRegister, transpile, execute, Aer
+from qiskit import QuantumCircuit, QuantumRegister,ClassicalRegister, transpile, execute, Aer
 
 from qiskit.visualization import plot_bloch_multivector
 from qiskit.tools.visualization import plot_histogram
@@ -15,16 +15,20 @@ def generate_board(dim, ships, boards):
     m = []
     for i in range(dim*dim):
         random_number = random.uniform(0, min(boards - boards/3 , target - current ))
-        m.append( random_number / boards)
+        m.append(random_number / boards)
         current += random_number
     random.shuffle(m)
+    print(m)
+    print(len(m))
+    print(sum(m))
     for i in range(dim):
         for j in range(dim):
             prob_0 = m[i*dim + j]
-            amplitude_0 = np.sqrt(prob_0)
-            amplitude_1 = np.sqrt(1 - prob_0)
+            amplitude_0 = np.sqrt(1 - prob_0)
+            amplitude_1 = np.sqrt(prob_0)
             qr = QuantumRegister(1, 'q')
-            circuit = QuantumCircuit(qr)
+            cr = ClassicalRegister(1, 'c')
+            circuit = QuantumCircuit(qr, cr)
             circuit.initialize([amplitude_0, amplitude_1], qr[0])
             b[i][j] = circuit
     return b
